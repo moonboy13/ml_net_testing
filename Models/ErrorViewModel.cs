dbd44edf07f87ff507513d4dd9c7944997ecbf89
Kyle Conlon <conlonkyle@gmail.com>
2023-12-22 21:19:10 -0500

    adding the template web project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ml_net_testing.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
